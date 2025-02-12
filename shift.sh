c1af7b972eda777a98915cba1abc22ebdee75602
sarmadali77771 <sarmad.ali77771@gmail.com>
2025-02-12 19:20:57 +0500

    Being shifty script - v1

---
+++ shift.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+>> commit
+beign shift
+eample run: ./shift.sh monday tuesday wednesday thursday friday
+commit 
+
+
+
+
+
+while [[ -n $1 ]];
+do 
+	echo "Each Paramater Shift to left : $1"
+	shift
+
+done
